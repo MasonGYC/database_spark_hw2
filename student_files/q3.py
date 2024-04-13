b1df37e4cab394fc9e6d789eaaf6e6a0b7aea4df
Mason <yuchen_guo@mymail.sutd.edu.sg>
2024-04-13 17:23:08 +0800

    Finished q3.

--- student_files/q3.py
+++ student_files/q3.py
@@ -1,38 +1,42 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, avg
+from pyspark.sql.functions import col, avg, lit
 
 # you may add more import if you need to
 
-# hdfs dfs -put E:\hw2\student_files\student_files\data\TA_restaurants_curated_cleaned.csv /assignment2/part1/input/
+# hdfs dfs -put ./student_files/data/TA_restaurants_curated_cleaned.csv /assignment2/part1/input/
+# spark-submit q3.py localhost
+# hadoop fs -ls -R / | grep "^d"
+# hdfs dfs -cat /assignment2/output/question3/*
 
 # don't change this line
 hdfs_nn = sys.argv[1]
+port = 9000
 
 spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
 
 # Read input data from HDFS
-input_path = "hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv" 
+input_path = f"hdfs://{hdfs_nn}:{port}/assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
 df = spark.read.csv(input_path, header=True, inferSchema=True)
 
 # Calculate average rating per restaurant and city
-avg_ratings = df.groupBy("City", "Restaurant").agg(avg("Rating").alias("AverageRating"))
+avg_ratings = df.groupBy("City", "Name").agg(avg("Rating").alias("AverageRating"))
 
 # Find top and bottom 3 cities based on average rating
 top_cities = avg_ratings.groupBy("City").avg("AverageRating").orderBy(col("avg(AverageRating)").desc()).limit(3)
 bottom_cities = avg_ratings.groupBy("City").avg("AverageRating").orderBy(col("avg(AverageRating)").asc()).limit(3)
 
 # Add 'RatingGroup' column to identify top and bottom cities
-top_cities = top_cities.withColumn("RatingGroup", "Top")
-bottom_cities = bottom_cities.withColumn("RatingGroup", "Bottom")
+top_cities = top_cities.withColumn("RatingGroup", lit("Top"))
+bottom_cities = bottom_cities.withColumn("RatingGroup", lit("Bottom"))
 
 # Combine top and bottom cities
-result = top_cities.union(bottom_cities).orderBy("City")
+result = top_cities.union(bottom_cities).withColumnRenamed("avg(AverageRating)", "AverageRating").orderBy(col("AverageRating").desc())
 
 # Write output to HDFS as CSV
-output_path = "hdfs://assignment2/output/question3".format(hdfs_nn)
-result.write.csv(output_path, header=True, mode="overwrite")
+output_path = f"hdfs://{hdfs_nn}:{port}/assignment2/output/question3/"
+result.coalesce(1).write.mode('overwrite').option('header','true').csv(output_path)
 
 # Stop Spark session
 spark.stop()
