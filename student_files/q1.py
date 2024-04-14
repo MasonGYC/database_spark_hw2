9b9d35898ee84107440911f7d85c472fd6fcf484
Mason <yuchen_guo@mymail.sutd.edu.sg>
2024-04-14 22:31:07 +0800

    finished q1 q2

--- student_files/q1.py
+++ student_files/q1.py
@@ -1,11 +1,26 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 # you may add more import if you need to
 
+# hdfs dfs -put ./student_files/data/TA_restaurants_curated_cleaned.csv /assignment2/part1/input/
+# spark-submit q1.py localhost
+# hadoop fs -ls -R / | grep "^d"
+# hdfs dfs -cat /assignment2/output/question1/*
 
 # don't change this line
 hdfs_nn = sys.argv[1]
+port = 9000
 
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
 
+input_path = f"hdfs://{hdfs_nn}:{port}/assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
+df = spark.read.csv(input_path, header=True, inferSchema=True)
+
+result = df.filter((col("Number of Reviews").isNotNull()) & (col("Rating") >= 1.0))
+
+output_path = f"hdfs://{hdfs_nn}:{port}/assignment2/output/question1/"
+result.coalesce(1).write.mode('overwrite').option('header','true').csv(output_path)
+
+spark.stop()
--- student_files/q2.py
+++ student_files/q2.py
@@ -1,11 +1,42 @@
 import sys
 from pyspark.sql import SparkSession
-
+from pyspark.sql.functions import col, max, min
 # you may add more import if you need to
 
 
+# hdfs dfs -put ./student_files/data/TA_restaurants_curated_cleaned.csv /assignment2/part1/input/
+# spark-submit q2.py localhost
+# hadoop fs -ls -R / | grep "^d"
+# hdfs dfs -cat /assignment2/output/question2/*
+# "_c0", "Name", "City", "Cuisine Style","Ranking", "Rating","Price Range","Number of Reviews","Reviews" ,"URL_TA","ID_TA"
+
 # don't change this line
 hdfs_nn = sys.argv[1]
+port = 9000
 
 spark = SparkSession.builder.appName("Assigment 2 Question 2").getOrCreate()
 # YOUR CODE GOES BELOW
+
+input_path = f"hdfs://{hdfs_nn}:{port}/assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
+df = spark.read.csv(input_path, header=True, inferSchema=True)
+
+df_filtered = df.filter(col("Price Range").isNotNull())
+
+df1 = df_filtered.groupBy("City", "Price Range").agg(max(col("Rating")).alias("Best Rating"), min(col("Rating")).alias("Worst Rating"))
+df1.show()
+
+best_res = df.join(df1, (df1["City"] == df_filtered["City"]) &
+                                             (df1["Price Range"] == df_filtered["Price Range"]) &
+                                             (df1["Best Rating"] == df_filtered["Rating"]), "leftsemi") \
+                                                  .dropDuplicates()
+worst_res = df.join(df1, (df1["City"] == df_filtered["City"]) &
+                                             (df1["Price Range"] == df_filtered["Price Range"]) &
+                                             (df1["Worst Rating"] == df_filtered["Rating"]), "leftsemi") \
+                                                  .dropDuplicates()
+
+result = best_res.union(worst_res).dropDuplicates().orderBy(col("City").asc())
+result.show()
+output_path = f"hdfs://{hdfs_nn}:{port}/assignment2/output/question2/"
+result.coalesce(1).write.mode('overwrite').option('header','true').csv(output_path)
+
+spark.stop()
